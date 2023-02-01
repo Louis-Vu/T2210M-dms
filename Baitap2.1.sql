1eb1e2e7f449e575803be28e417478eeadafe327
Louis-Vu <hoangvuhuy04@gmail.com>
2023-02-01 08:13:07 +0700

    Update Baitap2.1.sql

--- Baitap2.1.sql
+++ Baitap2.1.sql
@@ -65,54 +65,30 @@ values('Hiace','Toyota',16),
 select * from vehicles;
 
 insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK001','NCC0001','DV01','Hiace','MP01','20/11/2015','20/11/2016',4);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK002','NCC0002','DV02','Vios','MP02','20/11/2015','20/11/2017',3);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK003','NCC0003','DV03','Escape','MP03','20/11/2017','20/11/2018',5);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK004','NCC0005','DV01','Cerato','MP04','20/11/2015','20/11/2019',7);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK005','NCC0002','DV02','Forte','MP03','20/11/2019','20/11/2020',1);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK006','NCC0004','DV03','Starex','MP04','10/11/2016','20/11/2021',2);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK007','NCC0005','DV01','Cerato','MP03','30/11/2015','25/01/2016',8);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK008','NCC0006','DV01','Vios','MP02','28/02/2016','15/08/2016',9);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK009','NCC0005','DV03','Grand-i10','MP02','27/04/2016','30/04/2017',10);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0010','NCC0006','DV01','Forte','MP02','21/11/2015','22/02/2016',4);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0011','NCC0007','DV01','Forte','MP01','25/12/2016','20/02/2017',5);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0012','NCC0007','DV03','Cerato','MP01','14/104/2016','20/12/2017',6);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0013','NCC0003','DV02','Cerato','MP01','21/12/2015','21/12/2016',8);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0014','NCC0008','DV01','Cerato','MP01','20/05/2016','30/12/2016',1);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0015','NCC0003','DV03','Hiace','MP02','24/04/2018','20/11/2019',6);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0016','NCC0001','DV03','Grand-i10','MP02','22/06/2016','21/12/2016',8);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0017','NCC0002','DV03','Cerato','MP03','30/09/2016','30/09/2019',4);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0018','NCC0008','DV03','Escape','MP04','13/12/2017','30/09/2018',2);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0019','NCC0003','DV03','Escape','MP03','24/01/2016','30/12/2016',8);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0020','NCC0002','DV03','Cerato','MP04','03/05/2016','21/10/2017',7);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0021','NCC0006','DV01','forte','MP02','30/01/2015','30/12/2016',9);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0022','NCC0002','DV02','Cerato','MP04','25/07/2016','30/12/2017',6);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0023','NCC0002','DV01','Forte','MP03','30/11/2017','20/05/2018',5);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0024','NCC0003','DV03','Forte','MP04','23/12/2017','30/11/2019',8);
-insert into subs(id,pro_id,service_id,vehicles,charges_id,supply_start_date,offer_end_date,number_of_registered_vehicles)
-values('DK0025','NCC0003','DV03','Hiace','MP02','24/08/2016','25/10/2017',1);
+values('DK001','NCC001','DV01','Hiace','MP01','2015/11/20','2016/11/20',4),
+('DK002','NCC002','DV02','Vios','MP02','2015/11/20','2017/11/20',3),
+('DK003','NCC003','DV03','Escape','MP03','2017/11/20','2018/11/20',5),
+('DK004','NCC005','DV01','Cerato','MP04','2015/11/20','2019/11/20',7),
+('DK005','NCC002','DV02','Forte','MP03','2019/11/20','2020/11/20',1),
+('DK006','NCC004','DV03','Starex','MP04','2016/11/10','2016/11/20',2),
+('DK007','NCC005','DV01','Cerato','MP03','2015/11/30','2016/01/25',8),
+('DK008','NCC006','DV01','Vios','MP02','2016/02/28','2016/08/15',9),
+('DK009','NCC005','DV03','Grand-i10','MP02','2016/04/27','2017/04/30',10),
+('DK010','NCC006','DV01','Forte','MP02','2015/11/21','2016/02/22',4),
+('DK011','NCC007','DV01','Forte','MP01','2016/12/25','2017/02/20',5),
+('DK012','NCC007','DV03','Cerato','MP01','2016/04/14','2017/12/20',6),
+('DK013','NCC003','DV02','Cerato','MP01','2015/12/21','2016/12/21',8),
+('DK014','NCC008','DV02','Cerato','MP01','2016/05/20','2016/12/30',1),
+('DK015','NCC003','DV01','Hiace','MP02','2018/04/24','2019/11/20',6),
+('DK016','NCC001','DV03','Grand-i10','MP02','2016/06/22','2016/12/21',8),
+('DK017','NCC002','DV03','Cerato','MP03','2016/09/30','2019/09/30',4),
+('DK018','NCC006','DV03','Escape','MP04','2017/12/13','2018/09/30',2),
+('DK019','NCC003','DV03','Escape','MP03','2016/01/24','2016/12/30',8),
+('DK020','NCC002','DV03','Cerato','MP04','2016/05/03','2017/10/21',7),
+('DK021','NCC006','DV01','Forte','MP02','2015/01/30','2016/12/30',9),
+('DK022','NCC002','DV02','Cerato','MP04','2016/07/25','2017/12/30',6),
+('DK023','NCC002','DV01','Forte','MP03','2017/11/30','2018/05/20',5),
+('DK024','NCC003','DV03','Forte','MP04','2017/12/23','2019/11/30',8),
+('DK025','NCC003','DV03','Hiace','MP02','2016/08/24','2017/10/25',1);
 
 select * from subs
